0d8205e11e8cad71d6babf056d0971a95428e0f3
Yufeng Lai <yufenlai@amazon.com>
2023-12-01 07:45:58 +0000

    adjust parameters

--- design.r
+++ design.r
@@ -3,6 +3,13 @@ rm(list = ls())
 
 set.seed(1201202)
 at.lvls <- c(4, 3, 3, 4)
+
+### attribute levels
+# 1: price; 9, 12, 15, 19
+# 3: location: global, EU, UK
+# 3: certification; None, NGO, UK
+# 4: project: forest, renewable, storage, appliances
+
 c.type <- c("C", "D", "D", "D")
 cs <- Profiles(
     lvls = at.lvls,
@@ -10,13 +17,13 @@ cs <- Profiles(
     c.lvls = list(c(9, 12, 15, 19))
 )
 
-mu0 <- 1
+mu0 <- -1
 sigma0 <- diag(length(mu0))
-M0 <- MASS::mvrnorm(n = 500, mu = mu0, Sigma = sigma0)
+M0 <- MASS::mvrnorm(n = 1000, mu = mu0, Sigma = sigma0)
 
-mu1 <- c(-0.1, 0, 0, 0, 0, 0, 0, 0)
+mu1 <- c(-0.1, 0.1, 0.2, 0.1, 0.2, -0.1, -0.3, -0.2)
 sigma1 <- diag(length(mu1))
-M1 <- MASS::mvrnorm(n = 500, mu = mu1, Sigma = sigma1)
+M1 <- MASS::mvrnorm(n = 1000, mu = mu1, Sigma = sigma1)
 
 D <- Modfed(
     cand.set = cs,
@@ -27,3 +34,5 @@ D <- Modfed(
     no.choice = TRUE
 )
 D
+
+save.image("design.RData")
