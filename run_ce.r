b29fe2636cd133844ff95b5d97e885f3207f5012
Yufeng Lai <yufenlai@amazon.com>
2024-09-19 04:30:29 +0000

    add latent class and effect of demographics

--- run_ce.r
+++ run_ce.r
@@ -10,9 +10,11 @@ library(gtools)
 library(gmnl)
 library(mlogit)
 library(ordPens)
+library(nnet)
 
 rm(list = ls())
 set.seed(1201202)
+load("./output/dt_ce.RData")
 
 date <- gsub("-", "", as.character(Sys.Date()))
 ### sink message
@@ -29,153 +31,9 @@ sink(my_log, append = TRUE, type = "output")
 sink(my_log, append = TRUE, type = "message")
 
 ################################################################################
-### Read in data
-################################################################################
-data <- fread("./csv/data_bw.csv")
-data[, cid := .GRP, by = .(ResponseId, scenario)]
-
-### merge in demographics 
-dt_demo <- fread("csv/dt_demo.csv")
-
-### select useful columns
-dt_ctrl <- 
-    dt_demo %>%
-    select(
-        ResponseId,
-        location,
-        age,
-        gender,
-        diet,
-        education,
-        hh_size,
-        income,
-        n_children,
-        shopper,
-        income,
-        where_live,
-        co2_value,
-        framing_effect
-    )
-
-### principle component analysis for Q9, 10, 12
-# define the sequence of lambda
-lambda_seq <- exp(seq(5, -10, by = -0.1))
-# for Q9
-dt_q9 <- 
-    dt_demo %>%
-    select(starts_with("Q9")) %>%
-    mutate(
-        across(
-            starts_with("Q9_"),
-            ~ case_when(
-                .x == "Stronglyagree" ~ 1,
-                .x == "Agree" ~ 2,
-                .x == "Neutral" ~ 3,
-                .x == "Disagree" ~ 4,
-                .x == "Stronglydisagree" ~ 5
-            )
-        )
-    )
-
-pc_fit_q9 <-
-    dt_q9 %>%
-        ordPCA(
-            p = 2,
-            lambda = lambda_seq,
-            CV = TRUE,
-            k = 10,
-            CVfit = FALSE
-        )
-l_star <- lambda_seq[which.max(
-    apply(pc_fit_q9$VAFtest, 2, mean)
-)]
-
-pc_fit_q9_star <-
-    dt_q9 %>%
-    ordPCA(
-        p = 2,
-        lambda = l_star
-    )
-
-pc_q9_mat <- pc_fit_q9_star$X
-colnames(pc_q9_mat) <- paste0("Q9_PC", 1:2)
-
-#for Q10
-dt_q10 <- 
-    dt_demo %>%
-    select(starts_with("Q10")) %>%
-    mutate(
-        across(
-            starts_with("Q10_"),
-            ~ case_when(
-                .x == "AlmostAlways" ~ 1,
-                .x == "Often" ~ 2,
-                .x == "Sometimes" ~ 3,
-                .x == "Rarely" ~ 4,
-                .x == "Never" ~ 5
-            )
-        )
-    )
-
-pc_fit_q10 <-
-    dt_q10 %>%
-        ordPCA(
-            p = 2,
-            lambda = lambda_seq,
-            CV = TRUE,
-            k = 10,
-            CVfit = FALSE
-        )
-
-l_star <- lambda_seq[which.max(
-    apply(pc_fit_q10$VAFtest, 2, mean)
-)]
-
-pc_fit_q10_star <-
-    dt_q10 %>%
-    ordPCA(
-        p = 2,
-        lambda = l_star
-    )
-
-pc_q10_mat <- pc_fit_q10_star$X
-colnames(pc_q10_mat) <- paste0("Q10_PC", 1:2)
-
-### merge in the principle components
-dt_ctrl <-
-    cbind(
-        dt_ctrl,
-        pc_q9_mat,
-        pc_q10_mat
-    )
-
-### create dummy variables
-dt_ctrl <-
-    cbind(
-        dt_ctrl,
-        model.matrix(
-            ~ -1 + framing_effect,
-            data = dt_ctrl
-        )
-    )
-
-### combine demographics data with the main data
-data <- merge(data, dt_ctrl, by = "ResponseId")
-
-### define discrete choice data
-dt <- mlogit.data(
-    data,
-    choice = "y",
-    shape = "long",
-    alt.var = "alt",
-    chid.var = "cid",
-    id.var = "ResponseId"
-)
-
-################################################################################
 ### basic logit
 ################################################################################
-
+message("Running basic logit...")
 ### mlogit
 f <- formula(
     paste0(
@@ -196,7 +54,6 @@ summary(logit_bsc)
 
 ### mixed logit
 randpar <- c(
-    price = "n",
     location_EU = "n",
     location_UK = "n",
     certificate_NGO = "n",
@@ -218,11 +75,11 @@ mlogit_bsc <- gmnl(
     iterlim = 5000
 )
 summary(mlogit_bsc)
-
+save.image("./output/ce_est.RData")
 ################################################################################
 ### mixed logit + co2 consumption + framing effect
 ################################################################################
-
+message("Running mixed logit with co2 consumption and framing effect...")
 ### mixed logit with controls
 f <- formula(
     paste0(
@@ -242,20 +99,8 @@ f <- formula(
     )
 )
 
-### define random parameters
-randpar <- c(
-    price = "n",
-    location_EU = "n",
-    location_UK = "n",
-    certificate_NGO = "n",
-    certificate_UK = "n",
-    project_renewable = "n",
-    project_landfill = "n",
-    project_manure = "n"
-)
-
 ### define interactions
-mvarlist <- list(
+mvarlist_1 <- list(
     location_EU = c("co2_value", "framing_effectconsequence", "framing_effectMetOffice", "framing_effectUN"),
     location_UK = c("co2_value", "framing_effectconsequence", "framing_effectMetOffice", "framing_effectUN"),
     certificate_NGO = c("co2_value", "framing_effectconsequence", "framing_effectMetOffice", "framing_effectUN"),
@@ -272,7 +117,7 @@ mlogit_ctrl_1 <-
         data = dt,
         model = "mixl",
         ranp = randpar,
-        mvar = mvarlist,
+        mvar = mvarlist_1,
         R = 2000,
         panel = T,
         haltons = NA,
@@ -280,11 +125,11 @@ mlogit_ctrl_1 <-
         iterlim = 5000
     )
 summary(mlogit_ctrl_1)
-
+save.image("./output/ce_est.RData")
 ################################################################################
 ### mixed logit + co2 consumption + framing effect + principle components
 ################################################################################
-
+message("Running mixed logit with co2 consumption, framing effect, and principle components...")
 ### mixed logit with controls
 f <- formula(
     paste0(
@@ -308,20 +153,8 @@ f <- formula(
     )
 )
 
-### define random parameters
-randpar <- c(
-    price = "n",
-    location_EU = "n",
-    location_UK = "n",
-    certificate_NGO = "n",
-    certificate_UK = "n",
-    project_renewable = "n",
-    project_landfill = "n",
-    project_manure = "n"
-)
-
 ### define interactions
-mvarlist <- list(
+mvarlist_2 <- list(
     location_EU = c("co2_value", "framing_effectconsequence", "framing_effectMetOffice", "framing_effectUN", "Q9_PC1", "Q9_PC2", "Q10_PC1", "Q10_PC2"),
     location_UK = c("co2_value", "framing_effectconsequence", "framing_effectMetOffice", "framing_effectUN", "Q9_PC1", "Q9_PC2", "Q10_PC1", "Q10_PC2"),
     certificate_NGO = c("co2_value", "framing_effectconsequence", "framing_effectMetOffice", "framing_effectUN", "Q9_PC1", "Q9_PC2", "Q10_PC1", "Q10_PC2"),
@@ -338,7 +171,7 @@ mlogit_ctrl_2 <-
         data = dt,
         model = "mixl",
         ranp = randpar,
-        mvar = mvarlist,
+        mvar = mvarlist_2,
         R = 2000,
         panel = T,
         haltons = NA,
@@ -346,68 +179,185 @@ mlogit_ctrl_2 <-
         iterlim = 5000
     )
 summary(mlogit_ctrl_2)
+save.image("./output/ce_est.RData")
 
 ################################################################################
-### latent class model
+### latent class model + mixed logit
 ################################################################################
-f <- 
-    formula(
-        paste0(
-            "y ~ -1 +",
-            paste0(
-                names(dt)[5:13],
-                collapse = " + | 0 | 0 | 0 | 1"
+
+### without PCA as control
+f1 <- formula(
+    y ~ 
+    -1 +
+    I +
+    price + price:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN) +
+    location_EU + location_EU:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN) +
+    location_UK + location_UK:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN) +
+    certificate_NGO + certificate_NGO:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN) +
+    certificate_UK + certificate_UK:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN) +
+    project_renewable + project_renewable:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN) +
+    project_landfill + project_landfill:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN) +
+    project_manure + project_manure:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN)
+    | 0 | 0 | 0 | 1
+)
+
+### with PCA as control
+f2 <- formula(
+    y ~ 
+    -1 +
+    I +
+    price + price:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN + Q9_PC1 + Q9_PC2 + Q10_PC1 + Q10_PC2) +
+    location_EU + location_EU:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN + Q9_PC1 + Q9_PC2 + Q10_PC1 + Q10_PC2) +
+    location_UK + location_UK:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN + Q9_PC1 + Q9_PC2 + Q10_PC1 + Q10_PC2) +
+    certificate_NGO + certificate_NGO:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN + Q9_PC1 + Q9_PC2 + Q10_PC1 + Q10_PC2) +
+    certificate_UK + certificate_UK:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN + Q9_PC1 + Q9_PC2 + Q10_PC1 + Q10_PC2) +
+    project_renewable + project_renewable:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN + Q9_PC1 + Q9_PC2 + Q10_PC1 + Q10_PC2) +
+    project_landfill + project_landfill:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN + Q9_PC1 + Q9_PC2 + Q10_PC1 + Q10_PC2) +
+    project_manure + project_manure:(co2_value + framing_effectconsequence + framing_effectMetOffice + framing_effectUN + Q9_PC1 + Q9_PC2 + Q10_PC1 + Q10_PC2)
+    | 0 | 0 | 0 | 1
+)
+
+### Latent class model from 2 to 5 classes, without PCA
+lc_est_1 <-
+    lapply(
+        2:5,
+        function(q) {
+            ### run latent class model
+            lc_ctrl <-
+                tryCatch(
+                    {
+                        gmnl(
+                            f1,
+                            data = dt,
+                            model = "lc",
+                            Q = q,
+                            panel = TRUE,
+                            method = "bhhh",
+                            iterlim = 5000
+                        )
+                    },
+                    error = function(e) {
+                        "Failed"
+                    }
+                )
+            summary(lc_ctrl)
+            
+            ### if successful, estimate effect of demographics
+            if (class(lc_ctrl) == "character") {
+                Y <- lc_ctrl$Qir
+                X <- as.matrix(
+                    data %>%
+                        select(
+                            ResponseId,
+                            age_group35_54,
+                            age_group55_,
+                            is_women,
+                            diet_typeFlexitarian,
+                            diet_typeVegan_Vegetarian,
+                            education_levelDegree,
+                            education_levelPostgraduate,
+                            hh_size,
+                            income_level30_50k,
+                            income_level50_,
+                            n_children,
+                            is_shopper,
+                            where_liveRuralarea,
+                            where_liveTownorsuburb
+                        ) %>%
+                        distinct() %>%
+                        select(
+                            -ResponseId
+                        )
+                )
+
+                lc_ctrl_demo <-
+                    multinom(
+                        Y ~ X
+                    )
+                summary(lc_ctrl_demo)
+            } else {
+                lc_ctrl_demo <- "Failed"
+            }
+            
+            return(
+                list(
+                    lc_est = lc_ctrl,
+                    lc_demo = lc_ctrl_demo
+                )
             )
-        )
+        }
     )
 
-### 2 classes
-lc2 <- gmnl(
-    f,
-    data = dt,
-    model = "lc",
-    Q = 2,
-    panel = T,
-    method = "bhhh"
-)
-summary(lc2)
+### 2 classes, without PCA
+lc_est_2 <-
+    lapply(
+        2:5,
+        function(q) {
+            ### run latent class model
+            lc_ctrl <-
+                tryCatch(
+                    {
+                        gmnl(
+                            f2,
+                            data = dt,
+                            model = "lc",
+                            Q = q,
+                            panel = TRUE,
+                            method = "bhhh",
+                            iterlim = 5000
+                        )
+                    },
+                    error = function(e) {
+                        "Failed"
+                    }
+                )
+            summary(lc_ctrl)
 
-### 3 classes
-lc3 <- gmnl(
-    f,
-    data = dt,
-    model = "lc",
-    Q = 3,
-    panel = T,
-    method = "bhhh"
-)
-summary(lc3)
+            ### if successful, estimate effect of demographics
+            if (class(lc_ctrl) == "character") {
+                Y <- lc_ctrl$Qir
+                X <- as.matrix(
+                    data %>%
+                        select(
+                            ResponseId,
+                            age_group35_54,
+                            age_group55_,
+                            is_women,
+                            diet_typeFlexitarian,
+                            diet_typeVegan_Vegetarian,
+                            education_levelDegree,
+                            education_levelPostgraduate,
+                            hh_size,
+                            income_level30_50k,
+                            income_level50_,
+                            n_children,
+                            is_shopper,
+                            where_liveRuralarea,
+                            where_liveTownorsuburb
+                        ) %>%
+                        distinct() %>%
+                        select(
+                            -ResponseId
+                        )
+                ) else {
+                    lc_ctrl_demo <- "Failed"
+                }
 
-### 4 classes
-lc4 <- gmnl(
-    f,
-    data = dt,
-    model = "lc",
-    Q = 4,
-    panel = T,
-    method = "bhhh"
-)
-summary(lc4)
+                lc_ctrl_demo <-
+                    multinom(
+                        Y ~ X
+                    )
+                summary(lc_ctrl_demo)
+            }
 
-### 5 classes
-lc5 <- gmnl(
-    f,
-    data = dt,
-    model = "lc",
-    Q = 5,
-    panel = T,
-    method = "bhhh"
-)
-summary(lc5)
-
-################################################################################
-### save image
-################################################################################
+            return(
+                list(
+                    lc_est = lc_ctrl,
+                    lc_demo = lc_ctrl_demo
+                )
+            )
+        }
+    )
 save.image("./output/ce_est.RData")
 
 ################################################################################
